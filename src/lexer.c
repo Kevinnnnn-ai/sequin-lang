f74ad59258cfceb3a4ac5366cd3fc8eda6de4464
Kevinnnnn-ai <kevinwjie@gmail.com>
2026-07-17 16:45:44 -0500

    refactor: refactor ToString into RunLexer and set up for token list making

--- src/lexer.c
+++ src/lexer.c
@@ -1,60 +1,110 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <string.h>
 #include <stdlib.h>
 
-char* ToString(const char *source_code_path);
-
 enum TokenType {
-    KEYWORD_MISSION,
-    OPERATOR_ASSIGNMENT,
-    IDENTIFIER_MISSION_NAME,
-    DELIMITER_SEMICOLON,
+    KEYWORD,
+    FLAG,
+    IDENTIFIER,
+    COMMENT,
+    WHITESPACE,
+    NEWLINE,
+
+    LITERAL_STRING,
+    LITERAL_NUMBER,
+    LITERAL_TIMESTAMP,
+    LITERAL_DURATION,
+
+    DELIMITER_COLON,
+    DELIMITER_COMMA,
+    DELIMITER_OPEN_PARENTHESES,
+    DELIMITER_CLOSE_PARENTHESES,
+    DELIMITER_OPEN_BRACES,
+    DELIMITER_CLOSE_BRACES,
+
+    END_OF_FILE,
 };
 
 struct Token {
-    int type;
+    enum TokenType type;
     const char *lexeme;
+    int length;
     int line;
     int column;
 };
 
-char* ToString(const char *source_code_path) {
-    FILE *source_code = fopen(source_code_path, "r");
-    if (source_code == NULL) {
+struct TokenList {
+    struct Token *token_list;
+    int count;
+    int capacity;
+};
+
+struct TokenList* GetTokenList(const char *src_string, int src_length);
+struct TokenList* RunLexer(const char *src_string, int src_length);
+
+void AppendToken(struct TokenList *token_list, struct Token token) {
+    
+}
+
+struct TokenList* GetTokenList(const char *src_string, int src_length) {
+    struct TokenList token_list;
+    token_list.count = 0;
+    token_list.capacity = 0;
+
+    int position = 0;
+    int line = 1;
+    int column = 1;
+
+    while (position <= src_length) {
+        int lexeme_start = 0;
+        char character = src_string[position];
+
+        if (character == ' ' || character == '\t') {
+            column++;
+            position++;
+        }
+
+        switch (character) {
+            
+        }
+
+    }
+}
+
+struct TokenList* RunLexer(const char* src_path) {
+    FILE *src_file = fopen(src_path, "r");
+    if (src_file == NULL) {
         return NULL;
     }
 
-    if (fseek(source_code, 0, SEEK_END) != 0) {
-        fclose(source_code);
+    if (fseek(src_file, 0, SEEK_END) != 0) {
+        fclose(src_file);
         return NULL;
     }
 
-    long source_code_length = ftell(source_code);
-    if (source_code_length == -1) {
-        fclose(source_code);
+    long src_length = ftell(src_file);
+    if (src_length == -1) {
+        fclose(src_file);
         return NULL;
     }
 
-    rewind(source_code);
+    rewind(src_file);
 
-    char *buffer = malloc(source_code_length + 1);
-    if (buffer == NULL) {
-        fclose(source_code);
+    char *src_string = malloc(src_length + 1);
+    if (src_string == NULL) {
+        fclose(src_file);
         return NULL;
     }
 
-    size_t bytes_read = fread(buffer, sizeof(char), source_code_length, source_code);
-    buffer[bytes_read] = '\0';
-    fclose(source_code);
-    return buffer;
+    size_t bytes_read = fread(src_string, sizeof(char), src_length, src_file);
+    src_string[bytes_read] = '\0';
+    fclose(src_file);
+
+    return NULL;
 }
 
 int main() {
-    const char *source_code_path = "C:\\Users\\Admin\\files\\projects\\sequin-lang\\examples\\example_1.sqn";
-
-    const char *script_string = ToString(source_code_path);
-    printf("%s", script_string);
-    free(script_string);
-
-    return 0;
+    const char *src_path = "C:\\Users\\Admin\\files\\projects\\sequin-lang\\examples\\example_1.sqn";
+    RunLexer(src_path);
 }
